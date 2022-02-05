4f2e47a3435c8bbb814e630869fa73160b58bdde
surgendentist <contact@surgendentist.in>
2022-02-05 08:26:52 +0530

    removed graphql

---
+++ precache-manifest.d66c7cd49809b165436afb9749a4d006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67c810f0d99c893a38f4ae31e9016bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "585c031d56d13babba24",
+    "url": "/static/css/2.c02fccde.chunk.css"
+  },
+  {
+    "revision": "a7545c0b48eaa9dc7f86",
+    "url": "/static/css/main.de309e25.chunk.css"
+  },
+  {
+    "revision": "585c031d56d13babba24",
+    "url": "/static/js/2.ea9c0711.chunk.js"
+  },
+  {
+    "revision": "be3ad328946fb10270868fb0c96fd8ff",
+    "url": "/static/js/2.ea9c0711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7545c0b48eaa9dc7f86",
+    "url": "/static/js/main.05094367.chunk.js"
+  },
+  {
+    "revision": "26069d3126fa2124dfd0",
+    "url": "/static/js/runtime-main.0c30b566.js"
+  }
+]);
