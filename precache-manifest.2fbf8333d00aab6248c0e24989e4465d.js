5006e9503fecc26a3bd967bb19f7c2ae5edc4fc7
skgandikota <skgandikota@users.noreply.github.com>
2022-03-10 13:10:40 +0000

    deploy: 41eaf8833eaf0137072febac9e5dcef0ad28b44c

---
+++ precache-manifest.2fbf8333d00aab6248c0e24989e4465d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187d4a23f87fa20e17ed703baa7bda51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d2e117ef611439be2d8",
+    "url": "/static/css/2.2f4ac3c4.chunk.css"
+  },
+  {
+    "revision": "84a002d9b2bd81610324",
+    "url": "/static/css/main.98bb7627.chunk.css"
+  },
+  {
+    "revision": "8d2e117ef611439be2d8",
+    "url": "/static/js/2.e304a8c4.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/static/js/2.e304a8c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a002d9b2bd81610324",
+    "url": "/static/js/main.2c9d5d7f.chunk.js"
+  },
+  {
+    "revision": "09d5012cb3a2ea4b4a8f",
+    "url": "/static/js/runtime-main.ce045065.js"
+  }
+]);
